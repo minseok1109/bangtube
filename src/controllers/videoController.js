57bd5f02ca6cf2337151d4fff3bd3aa1cde81443
minseok1109 <park485201@naver.com>
2022-01-21 00:07:40 +0900

    add 404 page, edit upload, videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,27 +3,40 @@ import Video from "../models/Video.js";
 export const home = async (req, res) => {
   try {
     const videos = await Video.find({});
-    res.render("home", { pageTitle: "Hom e", videos });
+    res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     return res.render("error");
   }
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch");
+  const video = await Video.findById(id);
+  console.log(video);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not Found" });
+  }
+  return res.render("watch", { pageTitle: `${video.title}`, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not Found" });
+  }
   return res.render("editVideo", {
-    pageTitle: `Editing`,
+    pageTitle: `Edit ${video.title}`,
+    video,
   });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not Found" });
+  }
   return res.redirect(`/videos/${id}`);
 };
 
@@ -34,20 +47,28 @@ export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hastags } = req.body;
-  const video = new Video({
-    title,
-    description,
-    createdAt: Date.now(),
-    hastags: hastags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  console.log(video);
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      hastags: hastags
+        .split(",")
+        .map((word) => (word.startWith("#") ? word : `#${word}`)),
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
 
 export const deleteVideo = (req, res) => {
